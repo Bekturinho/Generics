67ccffad3d13a14bbbe099af7cb135ca457bccb8
bekturinho <ismanalievbektur4@gmail.com>
2024-02-14 07:12:30 -0800

    cx

--- Generics/main.swift
+++ Generics/main.swift
@@ -24,23 +24,23 @@ var a = 1
 //Написать функцию, которая принимает массив элементов любого сравниваемого типа и
 //возвращает максимальный (самый большой) элемент из массива.
 
-func findBiggestValue<T:Comparable>(_ a:[T]){
+func findBiggestValue<T:Comparable>(_ a:[T]) -> T{
    let b = a.sorted(by: {$0 > $1})
-    print(b[0])
+    return(b[0])
     
     
 }
 
 
-findBiggestValue([1212,3234,56567,6])
+print(findBiggestValue([1212,3234,56567,6]))
 
 //Написать функцию, которая принимает два массива элементов любого типа и возвращает
 //массив, состоящий из элементов обоих входных массивов.
 //Например, два массива [Int]: [1,2,5] и [7,9, 100]. При вызове функция должна возвращать:
 //(1,2,5,7,9,100).
 
-func combinateArrays<T>(a: [T], b: [T]){
+func combinateArrays<T>(a: [T], b: [T]) -> [T]{
     let total = a + b
-    print(total)
+    return total
 }
-combinateArrays(a: [32323,4545,454,5,454], b: [3434,34,3434,34])
+print(combinateArrays(a: [32323,4545,454,5,454], b: [3434,34,3434,34]))
